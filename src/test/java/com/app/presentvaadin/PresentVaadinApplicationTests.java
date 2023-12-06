ebc6f4b44b6ed14da29b38fe190776c349cfc7c0
denisovamaria <mvdenisova.work@gmail.com>
2023-12-06 19:51:04 +0300

    Initial commit

---
+++ src/test/java/com/app/presentvaadin/PresentVaadinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.presentvaadin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresentVaadinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
